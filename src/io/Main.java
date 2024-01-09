9ea6f10795655144d34266b04cdb93466f34c899
Robin Smits <smit0141@hz.nl>
2024-01-09 21:12:23 +0100

    baseStructure

---
+++ src/io/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        ConsoleWriter writer = new ConsoleWriter();
+        ConsoleReader reader = new ConsoleReader();
+        String hello = "hello";
+        writer.writeLine(hello);
+    }
+}
